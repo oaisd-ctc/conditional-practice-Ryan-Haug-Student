7b4270ab4ccd2f79ba6e0e99de8dca114224b515
Ryan-Haug-Student <ryha601@student.oaisd.org>
2024-10-08 18:52:44 +0000

    added every function

--- Program.cs
+++ Program.cs
@@ -1,16 +1,40 @@
 
+using System.Security.Cryptography.X509Certificates;
+
 public class Program
 {
     public static void Main(string[] args)
     {
+Console.Clear();
+
         FahrenheitToCelsius(68); // Test FahrenheitToCelsius Function with a random value
 
-        // Test your functions by calling them below:
-        // CheckForPositiveNegativeZero(-1); // This should output: "Your number is negative"
-        // CheckForPositiveNegativeZero(0); // This should output: "Your number is zero"
-        // CheckForPositiveNegativeZero(1); // This should output: "Your number is positive"
-        // ...
-        // ...
+        CheckForPositiveNegativeZero(1);
+        CheckForPositiveNegativeZero(0);
+        CheckForPositiveNegativeZero(-1);
+
+        FindMinimum(1, 2, 3);
+        FindMinimum(2, 1, 3);
+        FindMinimum(3, 2, 1);
+
+        FindMaximum(1, 2, 3);
+        FindMaximum(2, 1, 3);
+        FindMaximum(3, 2, 1);
+
+        IsDivisibleBy5(5);
+        IsDivisibleBy5(6);
+
+        CheckEvenOrOdd(2);
+        CheckEvenOrOdd(3);
+
+        CheckVowelOrConsonant('e');
+        CheckVowelOrConsonant('E');
+        CheckVowelOrConsonant('s');
+        CheckVowelOrConsonant('S');
+
+        DisplayDayOfWeek(0);
+        DisplayDayOfWeek(3);
+        DisplayDayOfWeek(6);
         // DisplayDayOfWeek(0); // This should output: "Thursday"
 
     }
@@ -23,4 +47,80 @@ public class Program
 
     // Define your functions below:
 
+    public static void CheckForPositiveNegativeZero(int number){
+        if(number > 0){
+            Console.WriteLine("this number is positive");
+        }
+        else if(number == 0){
+            Console.WriteLine("this number is zero");
+        }
+        else{
+            Console.WriteLine("this number is negative");
+        }
+    }
+
+    public static void FindMinimum(int num1, int num2, int num3){
+            if(num1 < num2 && num1 < num3){
+                Console.WriteLine("the minimum is " + num1);
+            }
+
+            else if(num2 < num1 && num2 < num3){
+                Console.WriteLine("the minimum is " + num2);
+            }
+
+            else{
+                Console.WriteLine("the minimum is " + num3);
+            }
+    }
+
+    public static void FindMaximum(int num1, int num2, int num3){
+            if(num1 > num2 && num1 > num3){
+                Console.WriteLine("the maximum is " + num1);
+            }
+
+            else if(num2 > num1 && num2 > num3){
+                Console.WriteLine("the maximum is " + num2);
+            }
+
+            else{
+                Console.WriteLine("the maximum is " + num3);
+            }
+    }
+
+    public static void IsDivisibleBy5(int number){
+        if((number % 5) == 0){
+            Console.WriteLine(number + " can be divided by 5");
+        }
+        else{
+            Console.WriteLine(number + " cannot be divided by 5");
+        }
+    }
+
+    public static void CheckEvenOrOdd(int number){
+        if((number % 2) == 0){
+            Console.WriteLine(number + " is even");
+        }
+        else{
+            Console.WriteLine(number + " is not even");
+        }
+    }
+
+    public static void CheckVowelOrConsonant(char letter){
+        if("aeiou".Contains(letter) || "AEIOU".Contains(letter)){
+            Console.WriteLine(letter + " is a Vowel");
+        }
+        else{
+            Console.WriteLine(letter + " Is a consonant");
+        }
+    }
+
+    public static void DisplayDayOfWeek(int dayCode){
+        if(dayCode == 0){Console.WriteLine("Sunday");}
+        if(dayCode == 1){Console.WriteLine("Monday");}
+        if(dayCode == 2){Console.WriteLine("Tuesday");}
+        if(dayCode == 3){Console.WriteLine("Wednesday");}
+        if(dayCode == 4){Console.WriteLine("Thursday");}
+        if(dayCode == 5){Console.WriteLine("Friday");}
+        if(dayCode == 6){Console.WriteLine("Saturday");}
+    }
 }
